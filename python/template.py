955321b5b3031810d6cca1b2d670b41bc491b7e7
James Durand <james.durand@alumni.msoe.edu>
2016-04-24 06:24:07 -0500

    Update python template

--- python/template.py
+++ python/template.py
@@ -1,10 +1,14 @@
+# pylint: disable=invalid-name, missing-docstring
+
+from __future__ import print_function
+
 import sys
 
 with open(sys.argv[1], 'r') as fh:
-	for line in fh.readlines():
-		line = line.strip()
-		if line == '':
-			continue
+    for line in fh.readlines():
+        line = line.strip()
+        if line == '':
+            continue
 
-		# TODO:
+        # TODO:
 
